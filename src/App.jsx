5c6d5286404ddf8e0a904723488de40dc4bce5cb
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-04-12 14:51:56 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <Contact />);
+    // <BrowserRouter>
+      {/* <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <div className="relative top-0 z-0"> */}
+          
+        {/* </div>
+      </div> */}
+    // </BrowserRouter>
+  
+};
+
+export default App;
